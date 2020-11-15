634fe17ce5c0d541adf769dc066240784389270a
James Winkle <james.winkle@ascendum.com>
2020-11-15 17:49:37 -0500

    tuples

--- basics.ts
+++ basics.ts
@@ -1,12 +1,10 @@
-const person = {
+const person : {name: string, age: number, hobbies: string[], roles: [number, string]}= {
     name: 'Maximilian',
     age: 30,
-    hobbies : ['sports', 'cooking']
+    hobbies : ['sports', 'cooking'],
+    roles: [1, 'admin']
 }
 
-// for(const hobby of person.hobbies){
-//     console.log(hobby)
-// }
 
 const hamArray = ['Hamilton', 'Burr', 'Jerfferson'];
 
@@ -15,3 +13,15 @@ for(const character of hamArray){
 }
 
 console.log(typeof hamArray)
+
+// typescript has a 'tuple'
+// in the above person.roles is a tuple with element one needing to be a number and element 2 needing to be a string
+
+// below throws error because 'hi' is not a number
+person.roles = ['hi', 'world'];
+
+// below throws error because length is greater than 2
+person.roles = [1, 'dev', 'hi'];
+
+// unfortunately, the push method is a method that is an exception of the tuple structure
+person.roles.push(4);
